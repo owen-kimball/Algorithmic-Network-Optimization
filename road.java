a9ee0fe31b8f460df4b706df8faf74c5510ac5c7
Owen Kimball <owenmkimball@gmail.com>
2026-09-05 12:53:47 -0700

    Update road.java

--- road.java
+++ road.java
@@ -1,1 +1,23 @@
+public class Road {
+  Location location1;
+  Location location2;
+  double travelTime;
 
+  Road(Location firstLocation, Location secondLocation, double timeToTravel) {
+    location1 = firstLocation;
+    location2 = secondLocation;
+    travelTime = timeToTravel;
+  }
+
+  public Location getLocation1() {
+    return location1;
+  }
+
+  public Location getLocation2() {
+    return location2;
+  }
+
+  public double getTravelTime() {
+    return travelTime;
+  }
+}
