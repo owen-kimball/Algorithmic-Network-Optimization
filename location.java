ae449a40f535d9ed509efea45c1d535b94e4348c
Owen Kimball <owenmkimball@gmail.com>
2026-09-05 12:45:16 -0700

    Update location.java

--- location.java
+++ location.java
@@ -1,1 +1,11 @@
+public class Location {
+  String nameOfLocation;
+  
+  Location(String nameProvided) {
+    nameOfLocation = nameProvided;
+  }
 
+  public String getLocationName() {
+    return nameOfLocation;
+  }
+}
