663a7c95fb9bf7f1427ac53065b208e064a38f10
Owen Kimball <owenmkimball@gmail.com>
2026-09-07 15:53:26 -0700

    Update graph.java

--- graph.java
+++ graph.java
@@ -19,13 +19,55 @@ public class Graph {
 
   public ArrayList<Road> listOfRoads(int roadAmount) {
     ArrayList<Road> roadList = new ArrayList<>();
-    int roadsLeft = numberOfRoads - numberOfLocations;
     ArrayList<Location> locationList = new ArrayList<>();
     locationList = listOfLocations(numberOfLocations);
     Collections.shuffle(locationList, exactSeed);
+
+    int roadsLeft = numberOfRoads - numberOfLocations;
+    double timeElapsed;
     int sizeOfLocationList = locationList.size() - 1;
+    Random timeGeneration = new Random();
+    
     for (int i = 0; i < sizeOfLocationList; i++) {
-      Road
+      timeElapsed = (timeGeneration.nextDouble() + 1.0) * 15.0;
+      Road newRoad = new Road(locationList.get(i), locationList.get(i + 1), timeElapsed);
+      roadList.add(newRoad);
+    }
+    
+    Random timeGeneration = new Random();
+    timeElapsed = (timeGeneration.nextDouble() + 1.0) * 15.0;
+    Road newRoad = new Road(locationList.get(sizeOfLocationList), locationList.get(0), timeElapsed);
+    roadList.add(newRoad);
+
+    while (roadsLeft > 0) {
+      Location location1;
+      Location location2;
+      int intForLocation1;
+      int intForLocation2;
+      
+      intForLocation1 = timeGeneration.nextInt(sizeOfLocationList);
+      intForLocation2 = intForLocation1;
+      while (intForLocation2 == intForLocation1) {
+        intForLocation2 = timeGeneration.nextInt(sizeOfLocationList);
+      }
+
+      timeElapsed = (timeGeneration.nextDouble() + 1.0) * 15.0;
+
+      Road newRoad = new Road(locationList.get(intForLocation1), locationList.get(intForLocation2), timeElapsed);
+      roadList.add(newRoad);
+      roadsLeft--;
+    }
+
+    return roadList;
+  }
+
+  Graph(ArrayList<Location> locationList) {
+    ArrayList<Road> roadList = new ArrayList<>();
+    roadList = listOfRoads(locationList);
+    int lengthOfLocationList = locationList.size();
+    
+    for (int i = 0; i < lengthOfLocationList; i++) {
+      
     }
   }
 }
