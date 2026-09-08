0fe2b22e2279170b5f600700a4f84650ed8ce8b4
Owen Kimball <owenmkimball@gmail.com>
2026-09-08 14:35:19 -0700

    Update location.java

--- location.java
+++ location.java
@@ -8,4 +8,19 @@ public class Location {
   public String getLocationName() {
     return nameOfLocation;
   }
+
+  public String toString(Location l) {
+    return l.getLocationName();
+  }
+
+  public boolean equals(Location l1, Location l2) {
+    String l1Name, l2Name;
+    l1Name = l1.toString();
+    l2Name = l2.toString();
+    if(l1Name.equals(l2Name)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
