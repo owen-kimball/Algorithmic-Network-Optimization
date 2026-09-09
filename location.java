f3f22beb69b527f4a2a543700bd78035c6ec384a
Owen Kimball <owenmkimball@gmail.com>
2026-09-09 15:41:25 -0700

    Update location.java

--- location.java
+++ location.java
@@ -3,24 +3,23 @@ public class Location {
   
   public Location(String nameOfLocation) {
     this.nameOfLocation = nameOfLocation;
-  }
+  } // Creates a Location.
 
-  public String getLocationName() {
-    return nameOfLocation;
-  }
+  @Override
+  public String toString() {
+    return this.nameOfLocation;
+  } // Returns the name of the Location.
 
-  public String toString(Location l) {
-    return l.getLocationName();
-  }
-
-  public boolean equals(Location l1, Location l2) {
-    String l1Name, l2Name;
-    l1Name = l1.toString();
-    l2Name = l2.toString();
-    if (l1Name.equals(l2Name)) {
-      return true;
-    } else {
-      return false;
+  public boolean equals(Object o) {
+    if (o != null) {
+      String l1Name, l2Name;
+      l1Name = this.nameOfLocation;
+      l2Name = l2.toString();
+      if (l1Name.equals(l2Name)) {
+        return true;
+      } else {
+        return false;
+      }
     }
-  }
+  } // Checks if two locations are the same.
 }
