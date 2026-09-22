dd60b987c18c80cba4cec3a808dd9fffa49da84c
Owen Kimball <owenmkimball@gmail.com>
2026-09-22 14:07:31 -0700

    Update location.java

--- location.java
+++ location.java
@@ -8,18 +8,28 @@ public class Location {
   @Override
   public String toString() {
     return this.nameOfLocation;
+  } // Prints the name of the Location when converting to String.
+
+  public String getLocationName() {
+    return this.nameOfLocation;
   } // Returns the name of the Location.
 
+  @Override
   public boolean equals(Object o) {
-    if (o != null) {
-      String l1Name, l2Name;
-      l1Name = this.nameOfLocation;
-      l2Name = l2.toString();
-      if (l1Name.equals(l2Name)) {
-        return true;
-      } else {
-        return false;
+    if (o == this) {
+      return true;
+    } // Checks if objects are exact same.
+    else if (o != null) { // Checks to make sure Object o is not null.
+      if (o instanceof Location) { // Checks to make sure Object o is an instance of a Location object.
+        Location locObj = (Location) o; // Casts o to Location object locObj.
+        return this.nameOfLocation.equals(locObj.getLocationName()); // Returns the final equality.
       }
     }
+    return false;
   } // Checks if two locations are the same.
+
+  @Override
+  public int hashCode() {
+    return this.nameOfLocation.hashCode();
+  } // Provides the hashCode capability.
 }
